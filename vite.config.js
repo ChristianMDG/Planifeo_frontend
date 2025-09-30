49216a7fb52e8d079323f90fdf9e230a02f2abd4
ChristianMDG <christian.ravelojaona@passerellesnumeriques.org>
2025-09-30 12:21:14 +0300

    add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(),tailwwindcss()],
+  plugins: [react(),tailwindcss()],
   server: {
     port: 3001,
     proxy: mode === 'development' ? {
