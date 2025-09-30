9364f57bf18adc549d7ffb39b4b1c10970121433
ChristianMDG <christian.ravelojaona@passerellesnumeriques.org>
2025-09-30 14:34:01 +0300

    final commit

--- src/services/api/config.js
+++ src/services/api/config.js
@@ -1,5 +1,4 @@
-// Configuration de l'API
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL,
+  BASE_URL: '/api', // fonctionne local et sur Render
   TIMEOUT: 10000
 };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(() => {
+  const apiUrl = 'https://planifeo-backend.onrender.com'; // backend deployé sur Render
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3001,
+      proxy: {
+        '/api': {
+          target: apiUrl,
+          changeOrigin: true,
+
+        }
+      }
+    }
+  };
 });
